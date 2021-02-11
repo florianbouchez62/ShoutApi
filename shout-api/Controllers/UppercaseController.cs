06af82f5ed27768ff1cd4d781d4cae23c31e4f57
Florian BOUCHEZ <florian.bouchez@FIMAGRP.GROUP>
2021-02-11 09:14:28 +0100

    Ajoutez des fichiers projet.

---
+++ shout-api/Controllers/UppercaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace shout_api.Controllers
+{
+    [Route("api/uppercase")]
+    [ApiController]
+    public class UppercaseController : ControllerBase
+    {
+        // POST api/<UppercaseController>
+        [HttpPost]
+        public Uppercase Post(Uppercase uppercase)
+        {
+            return uppercase;
+        }
+    }
+}
---
+++ shout-api/Uppercase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shout_api
+{
+    public class Uppercase
+    {
+        public string Word { get; set; }
+    }
+}
